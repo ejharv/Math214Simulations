c37eec25d7edf31d985abb2a55ae246911c90870
ejharv <116828943+ejharv@users.noreply.github.com>
2024-04-22 08:31:28 -0400

    Add files via upload

---
+++ sphere.py
@@ -0,0 +1,80 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Sphere:
+    def __init__(self, center, radius, color):
+        self.center = np.array(center)
+        self.radius = radius
+        self.color = np.array(color)
+
+    def intersect(self, ray_origin, ray_direction):
+        oc = ray_origin - self.center
+        a = np.dot(ray_direction, ray_direction)
+        b = 2.0 * np.dot(oc, ray_direction)
+        c = np.dot(oc, oc) - self.radius ** 2
+        discriminant = b ** 2 - 4 * a * c
+        if discriminant < 0:
+            return None, None
+        sqrt_d = np.sqrt(discriminant)
+        t1 = (-b - sqrt_d) / (2 * a)
+        t2 = (-b + sqrt_d) / (2 * a)
+        if t1 > 0 and t2 > 0:
+            min_t = min(t1, t2)
+            return min_t, (ray_origin + min_t * ray_direction - self.center) / self.radius
+        return None, None
+
+class Light:
+    def __init__(self, position, intensity):
+        self.position = np.array(position)
+        self.intensity = intensity
+
+def compute_lighting(point, normal, scene_light):
+    light_dir = scene_light.position - point
+    light_dir /= np.linalg.norm(light_dir)
+    return max(np.dot(light_dir, normal), 0) * scene_light.intensity
+
+def ray_from_camera(camera_position, x, y, image_width, image_height, fov):
+    aspect_ratio = image_width / image_height
+    scale = np.tan(fov * 0.5 * np.pi / 180)
+    pixel_x = (2 * (x + 0.5) / image_width - 1) * aspect_ratio * scale
+    pixel_y = (1 - 2 * (y + 0.5) / image_height) * scale
+    ray_direction = np.array([pixel_x, pixel_y, -1])
+    ray_direction /= np.linalg.norm(ray_direction)
+    return camera_position, ray_direction
+
+def render(scene, scene_light, camera_position, image_width, image_height, fov):
+    image = np.zeros((image_height, image_width, 3))
+    for y in range(image_height):
+        for x in range(image_width):
+            origin, direction = ray_from_camera(camera_position, x, y, image_width, image_height, fov)
+            closest_obj = None
+            min_dist = np.inf
+            for obj in scene:
+                dist, normal = obj.intersect(origin, direction)
+                if dist is not None and dist < min_dist:
+                    min_dist = dist
+                    closest_obj = obj
+                    hit_normal = normal
+            if closest_obj:
+                hit_point = origin + min_dist * direction
+                light_intensity = compute_lighting(hit_point, hit_normal, scene_light)
+                color = closest_obj.color * light_intensity
+                image[y, x] = np.clip(color, 0, 1)
+    return image
+
+# Scene setup
+scene = [Sphere([0, 0, -5], 1, [1, 0, 0])]  # Single red sphere
+scene_light = Light([1, -1, -3], 1.0)  # Light setup
+camera_position = np.array([0, 0, 0])  # Camera setup
+image_width = 800
+image_height = 600
+fov = 90
+
+# Render the scene
+image = render(scene, scene_light, camera_position, image_width, image_height, fov)
+
+# Display the result using matplotlib
+plt.figure(figsize=(10, 7.5))
+plt.imshow(image)
+plt.axis('off')
+plt.show()
